b8c465b9d2ba0cf4b4344c4ff925691b26f9f421
arreche <arreche@gmail.com>
2020-05-20 00:00:00 +0100

    09 - Special Operations

---
+++ 09/calculator.go
@@ -0,0 +1,17 @@
+// Package calculator provides math operations.
+package calculator
+
+// Substract returns the substraction of two integer numbers.
+func Substract(a, b int) int {
+	return a - b
+}
+
+// Multiply returns the multiplication of two integer numbers.
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide returns the division of two integer numbers.
+func Divide(a, b int) int {
+	return a / b
+}
--- 09/calculator_test.go
+++ 09/calculator_test.go
@@ -1,17 +1,63 @@
 package calculator_test
 
 import (
+	"calculator"
 	"testing"
 )
 
 func TestSubtract(t *testing.T) {
-	t.Fatal("Not implemented yet")
+	testCases := []struct {
+		a, b int
+		want int
+	}{
+		{a: 1, b: 1, want: 0},
+		{a: -1, b: -2, want: 1},
+		{a: -1, b: 2, want: -3},
+		{a: 1, b: 0, want: 1},
+		{a: 0, b: 0, want: 0},
+	}
+	for _, testCase := range testCases {
+		got := calculator.Substract(testCase.a, testCase.b)
+		if testCase.want != got {
+			t.Errorf("Subtract(%d, %d) want %d, got %d", testCase.a, testCase.b, testCase.want, got)
+		}
+	}
 }
 
 func TestMultiply(t *testing.T) {
-	t.Fatal("Not implemented yet")
+	testCases := []struct {
+		a, b int
+		want int
+	}{
+		{a: 2, b: 3, want: 6},
+		{a: 2, b: 1, want: 2},
+		{a: 2, b: 0, want: 0},
+		{a: 2, b: -1, want: -2},
+		{a: -2, b: -2, want: 4},
+	}
+	for _, testCase := range testCases {
+		got := calculator.Multiply(testCase.a, testCase.b)
+		if testCase.want != got {
+			t.Errorf("Multiply(%d, %d) want %d, got %d", testCase.a, testCase.b, testCase.want, got)
+		}
+	}
 }
 
 func TestDivide(t *testing.T) {
-	t.Fatal("Not implemented yet")
+	testCases := []struct {
+		a, b int
+		want int
+	}{
+		{a: 4, b: 2, want: 2},
+		{a: 2, b: 2, want: 1},
+		{a: 2, b: -1, want: -2},
+		{a: -2, b: -1, want: 2},
+		{a: -2, b: 1, want: -2},
+	}
+	for _, testCase := range testCases {
+		got := calculator.Divide(testCase.a, testCase.b)
+		if testCase.want != got {
+			t.Errorf("Divide(%d, %d) want %d, got %d", testCase.a, testCase.b, testCase.want, got)
+		}
+	}
 }
